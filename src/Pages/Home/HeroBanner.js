09b4ac4c42548c81c831b5fe893a8edeec2ba341
LAB <C-XIX@MBP-PANA.local>
2022-05-30 17:51:10 +0300

    + edits

--- src/Pages/Home/HeroBanner.js
+++ src/Pages/Home/HeroBanner.js
@@ -4,7 +4,7 @@ import React from "react";
 import { useNavigate } from "react-router-dom";
 import { GradientButtonPrimary } from "../../Utils/GradientButtons/GradientButtons";
 import CardsDeck from "./CardDeck";
-import opensea from "./CreateAssets/opensea.html";
+
 
 import heroBannerStyles from "./heroBannerStyles.module.css";
 import MobileCardDeck from "./MobileCardDeck";
