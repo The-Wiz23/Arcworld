22d8324238e5fbfb63bb103417fa80f235a7d176
LAB <C-XIX@MBP-PANA.local>
2022-06-03 15:10:15 +0300

    edit line:126 MobileNav

--- src/components/Navigation/MobileNavigation.js
+++ src/components/Navigation/MobileNavigation.js
@@ -123,7 +123,7 @@ const MobileNavigation = ({
                   alignItems: "center",
                   gap: 1,
                 }}
-                onClick={() => navigate("/auction?type=live")}
+                onClick={() => window.location.href = "https://opensea.io/collection/ggrogilla-the-great"}
                 variant="text"
                 color="secondary"
               >
